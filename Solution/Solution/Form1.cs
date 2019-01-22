55538bb51f62cdac6608b590afda93002dad91b5
Bilger Yahov <bilger.yahov@progress.com>
2019-01-22 11:34:28 +0200

    Connect to Kinvey backend.

--- Solution/Solution/Form1.cs
+++ Solution/Solution/Form1.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Kinvey;
 
 namespace Solution
 {
     public partial class Form1 : Form
     {
+        private Client.Builder mBuilder;
+        private Client mKinveyClient;
+
+        private String appKey = "xxx";
+        private String appSecret = "xxx";
+        private String authServiceID = "xxx";
+        private String instanceID = "xxx";
+
         public Form1()
         {
             InitializeComponent();
+
+            mBuilder = new Client.Builder(appKey, appSecret).SetInstanceID(instanceID).setLogger(Console.WriteLine);
+            mKinveyClient = mBuilder.Build();
+            KinveyPing();
         }
+
+        private async void KinveyPing()
+        {
+            try
+            {
+                PingResponse mPingResponse = await mKinveyClient.PingAsync();
+                MessageBox.Show("Kinvey Ping Response: " + mPingResponse.kinvey);
+            }
+            catch (Exception mExc)
+            {
+                MessageBox.Show("Kinvey Ping Exception: " + mExc.Message);
+            }
+
+        }
+        
+            
+
     }
 }
